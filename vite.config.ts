61ea520ca5001df96e1c1b8052cdb85aeefda40e
yatindeveloper2011 <yatindeveloper2011@gmail.com>
2026-06-02 12:11:04 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({mode}) => {
     base: '/',
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
